9b3e5b703a6ae1d5b2da6232716853f3133930d6
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-01-31 16:55:03 -0500

    got user from firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const {initializeApp, cert} = require("firebase-admin/app")
+const {getFirestore} = require("firebase-admin/firestore")
+
+const credentials = require("../credentials.json");
+
+initializeApp({
+    credential: cert(credentials),
+})
+const db = getFirestore();;
+
+
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (request, response) => {
-    response.send('Hello World!');
+    const userCollection = db.collection("users");
+    userCollection.get().then(snapshot => {
+        response.send(snapshot.docs)
+    })
+
+
+    //response.send('Hello World!');
+    
 })
 
 app.post('/users', (request, response) => {
